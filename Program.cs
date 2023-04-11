d4bb2cbc4a507e1cffae8102b75786722407e583
rakmate9812 <rakmate9812@gmail.com>
2023-04-11 10:50:39 +0200

    new-katas

--- Program.cs
+++ Program.cs
@@ -33,13 +33,27 @@ public static class Kata55d24f55d7dd296eb9000030 // summing words from 0 to num
     }
 }
 
+public static class Kata52fba66badcd10859f00097e // replace vowels in a string
+{
+    public static string Disemvowel(string str)
+    {
+        string[] vowels = { "a", "e", "i", "o", "u", "A", "E", "I", "O", "U" };
+        foreach (var letter in vowels)
+        {
+            str = str.Replace(letter, "");
+        }
+        return str;
+    }
+}
+
 class Program
 {
     static void Main()
     {
         // string result = Kata523b4ff7adca849afe000035.greet();
         // string result = Kata53dc23c68a0c93699800041d.Smash(new string[] { "this", "is", "a", "really", "long", "sentence" });
-        int result = Kata55d24f55d7dd296eb9000030.summation(8);
+        // int result = Kata55d24f55d7dd296eb9000030.summation(8);
+        string result = Kata52fba66badcd10859f00097e.Disemvowel("Hi this is Mate");
         Console.WriteLine(result);
     }
 }
