2d16acc0ea0705dfbc8656183d098483dd018bbf
rakmate9812 <rakmate9812@gmail.com>
2023-04-11 17:22:19 +0200

    new-kata

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿// using .NET 7 
 using System;
+using System.Linq;
 
 public static class Kata523b4ff7adca849afe000035
 {
@@ -46,6 +47,25 @@ public static class Kata52fba66badcd10859f00097e // replace vowels in a string
     }
 }
 
+public static class Kata546f922b54af40e1e90001da // getting the position of the letter in the alphabet - this works in .net 7, but not in the codewars enviroment
+{
+    public static string AlphabetPosition(string text)
+    {
+        string[] lowercaseLetters = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+        string modifiedText = text.ToLower();
+        string res = "";
+        for (int i = 0; i < modifiedText.Count(); i++)
+        {
+            if (Array.IndexOf(lowercaseLetters, modifiedText[i].ToString()) > -1) // 
+            {
+                res += Array.IndexOf(lowercaseLetters, modifiedText[i].ToString()) + 1; // need +1 because of indext starting at 0 in arrays
+                res += " ";
+            }
+        }
+        return res.TrimEnd();
+    }
+}
+
 class Program
 {
     static void Main()
@@ -53,7 +73,8 @@ class Program
         // string result = Kata523b4ff7adca849afe000035.greet();
         // string result = Kata53dc23c68a0c93699800041d.Smash(new string[] { "this", "is", "a", "really", "long", "sentence" });
         // int result = Kata55d24f55d7dd296eb9000030.summation(8);
-        string result = Kata52fba66badcd10859f00097e.Disemvowel("Hi this is Mate");
+        // string result = Kata52fba66badcd10859f00097e.Disemvowel("Hi this is Mate");
+        string result = Kata546f922b54af40e1e90001da.AlphabetPosition("The sunset sets at twelve o' clock.");
         Console.WriteLine(result);
     }
 }
